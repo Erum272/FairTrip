15406f6427dd556c7933f28fdea833cd5dde0ed4
Herum shaikh <erum20203@gmail.com>
2026-03-06 20:46:55 +0530

    Final update: added currency converter, improved UI, scam alert and updated navigation

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,11 @@
+window.onload=function(){
+
+alert("Tip: Always confirm prices before buying products.");
+
+};
+
+
+
 function checkPrice(){
 
 let product=document.getElementById("product").value.toLowerCase();
@@ -8,21 +16,20 @@ let prices={
 "coconut":"₹40 - ₹60",
 "souvenir":"₹150 - ₹300",
 "tshirt":"₹300 - ₹500",
-"coffee":"₹100 - ₹200"
+"coffee":"₹100 - ₹200",
+"tea":"₹10 - ₹20"
 
 };
 
 if(prices[product]){
 
-document.getElementById("result").innerHTML=
-"Average Local Price: "+prices[product];
+document.getElementById("result").innerHTML="Average Local Price: "+prices[product];
 
 }
 
 else{
 
-document.getElementById("result").innerHTML=
-"Price data not available.";
+document.getElementById("result").innerHTML="Price data not available.";
 
 }
 
@@ -35,11 +42,25 @@ function estimateFare(){
 let distance=document.getElementById("distance").value;
 
 let baseFare=50;
+
 let perKm=15;
 
 let total=baseFare+(distance*perKm);
 
-document.getElementById("fare").innerHTML=
-"Estimated Fare: ₹"+total;
+document.getElementById("fare").innerHTML="Estimated Fare: ₹"+total;
+
+}
+
+
+
+function convertCurrency(){
+
+let usd=document.getElementById("usd").value;
+
+let rate=83;
+
+let inr=usd*rate;
+
+document.getElementById("currencyResult").innerHTML="₹ "+inr;
 
 }
