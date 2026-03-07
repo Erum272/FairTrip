52a26cedc3708f4094671a20e27432e4ba1f5705
Herum shaikh <erum20203@gmail.com>
2026-03-07 23:56:20 +0530

    Added database models for price, vendor and scam reports

--- backend/models/Price.js
+++ backend/models/Price.js
@@ -1,27 +1,22 @@
 const mongoose = require("mongoose");
 
 const priceSchema = new mongoose.Schema({
-
-productName:{
-type:String,
-required:true
+productName: {
+type: String,
+required: true
 },
-
-averagePrice:{
-type:Number,
-required:true
+averagePrice: {
+type: Number,
+required: true
 },
-
-currency:{
-type:String,
-default:"INR"
+currency: {
+type: String,
+default: "INR"
 },
-
-location:{
-type:String,
-default:"India"
+location: {
+type: String,
+default: "India"
 }
-
 });
 
-module.exports = mongoose.model("Price",priceSchema);
+module.exports = mongoose.model("Price", priceSchema);
